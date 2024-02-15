1c071b6da8c178c84c01350384acd1810aed2608
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2024-02-15 23:24:59 +0300

    remove unused lsp keymappings

--- lua/huseyineergin/plugins/lsp.lua
+++ lua/huseyineergin/plugins/lsp.lua
@@ -13,7 +13,6 @@ return {
 		end,
 		lazy = true,
 		init = function()
-			-- Disable automatic setup, we are doing it manually
 			vim.g.lsp_zero_extend_cmp = 0
 			vim.g.lsp_zero_extend_lspconfig = 0
 		end,
@@ -93,35 +92,9 @@ return {
 			lsp_zero.extend_lspconfig()
 
 			lsp_zero.on_attach(function(_, bufnr)
-				-- lsp_zero.buffer_autoformat()
 				local opts = { buffer = bufnr, remap = false }
 				vim.keymap.set("n", "K", function()
 					vim.lsp.buf.hover()
-				end, opts)
-
-				vim.keymap.set("n", "gd", function()
-					vim.lsp.buf.definition()
-				end, opts)
-				vim.keymap.set("n", "]d", function()
-					vim.diagnostic.goto_prev()
-				end, opts)
-				vim.keymap.set("n", "[d", function()
-					vim.diagnostic.goto_next()
-				end, opts)
-				vim.keymap.set("n", "<leader>vrn", function()
-					vim.lsp.buf.rename()
-				end, opts)
-				vim.keymap.set("n", "<leader>vrr", function()
-					vim.lsp.buf.references()
-				end, opts)
-				vim.keymap.set("n", "<leader>vca", function()
-					vim.lsp.buf.code_action()
-				end, opts)
-				vim.keymap.set("n", "<leader>vd", function()
-					vim.diagnostic.open_float()
-				end, opts)
-				vim.keymap.set("n", "<leader>vws", function()
-					vim.lsp.buf.workspace_symbol()
 				end, opts)
 			end)
 
