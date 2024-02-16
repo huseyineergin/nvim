19e9a3641d01c976cb9e4295c1fb82c1a3fe4f20
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2024-02-16 18:48:50 +0300

    add lsp keymap

--- lua/huseyineergin/plugins/lsp.lua
+++ lua/huseyineergin/plugins/lsp.lua
@@ -93,8 +93,12 @@ return {
 
 			lsp_zero.on_attach(function(_, bufnr)
 				local opts = { buffer = bufnr, remap = false }
+
 				vim.keymap.set("n", "K", function()
 					vim.lsp.buf.hover()
+				end, opts)
+				vim.keymap.set("n", "gd", function()
+					vim.lsp.buf.definition()
 				end, opts)
 			end)
 
